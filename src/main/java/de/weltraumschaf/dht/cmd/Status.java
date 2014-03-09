af32cdb21b015fea58a5041fd0133da19b4f3327
Sven Strittmatter <ich@weltraumschaf.de>
2014-03-09 23:22:35 +0100

    Add message boxes to status.

--- src/main/java/de/weltraumschaf/dht/cmd/Status.java
+++ src/main/java/de/weltraumschaf/dht/cmd/Status.java
@@ -9,11 +9,13 @@
  *
  * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
  */
-
 package de.weltraumschaf.dht.cmd;
 
+import de.weltraumschaf.dht.msg.MessageBox;
+import de.weltraumschaf.dht.server.Server;
 import de.weltraumschaf.dht.shell.CommandMainType;
 import static de.weltraumschaf.dht.shell.CommandMainType.STATUS;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * Executes `status` command.
@@ -22,20 +24,8 @@ import static de.weltraumschaf.dht.shell.CommandMainType.STATUS;
  */
 final class Status extends BaseCommand {
 
-    @Override
-    public void execute() {
-        final StringBuilder buffer = new StringBuilder();
-
-        buffer.append("Node:").append(nl());
-        buffer.append("  Id:      ").append(getApplication().getNodeId().asString()).append(nl());
-
-        buffer.append("Server:").append(nl());
-        buffer.append("  State:   ").append(getApplication().getServer().getState()).append(nl());
-        buffer.append("  Address: ").append(formatLocalAddress()).append(nl());
-        buffer.append("  Queue:   ").append(getApplication().getServer().countQueue()).append(nl());
-
-        println(buffer.toString());
-    }
+    private static final String LEFT_PAD = "  ";
+    private static final int RIGHT_PAD = 10;
 
     @Override
     public Descriptor getDescriptor() {
@@ -48,7 +38,7 @@ final class Status extends BaseCommand {
 
             @Override
             public String getUsage() {
-                return "status";
+                return STATUS.toString();
             }
 
             @Override
@@ -56,6 +46,58 @@ final class Status extends BaseCommand {
                 return "Show application status information.";
             }
         };
+    }
+
+    @Override
+    public void execute() {
+        final StringBuilder buffer = new StringBuilder();
+        nodeInfo(buffer);
+        serverInfo(buffer);
+        messageInfos(buffer);
+        println(buffer.toString());
+    }
+
+    private String rightPad(final String input) {
+        return StringUtils.rightPad(input, RIGHT_PAD);
+    }
+
+    private String getNodeId() {
+        return getApplication().getNodeId().asString();
+    }
+
+    private Server getServer() {
+        return getApplication().getServer();
+    }
+
+    private MessageBox getOutbox() {
+        return getApplication().getOutbox();
+    }
+
+    private MessageBox getInbox() {
+        return getApplication().getInbox();
+    }
+
+    private void nodeInfo(final StringBuilder buffer) {
+        buffer.append("Node:").append(nl());
+        buffer.append(LEFT_PAD).append(rightPad("Id:")).append(getNodeId()).append(nl());
+    }
+
+    private void serverInfo(final StringBuilder buffer) {
+        buffer.append("Server:").append(nl());
+        buffer.append(LEFT_PAD).append(rightPad("State:")).append(getServer().getState()).append(nl());
+        buffer.append(LEFT_PAD).append(rightPad("Address:")).append(formatLocalAddress()).append(nl());
+        buffer.append(LEFT_PAD).append(rightPad("Queue:")).append(getServer().countQueue()).append(nl());
+    }
+
+    private void messageInfos(final StringBuilder buffer) {
+        buffer.append("Message:").append(nl());
+        buffer.append(LEFT_PAD).append(rightPad("Inbox:")).append(formatInbox()).append(nl());
+        buffer.append(LEFT_PAD).append(rightPad("Outbox:")).append(getOutbox().count()).append(nl());
+    }
+
+    private String formatInbox() {
+        final MessageBox inbox = getInbox();
+        return String.format("%d (%d unread)", inbox.count(), inbox.countUnread());
     }
 
 }
