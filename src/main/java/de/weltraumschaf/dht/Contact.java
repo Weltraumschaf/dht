43c412990f621d2f8c00aac6ff6001fe39933198
Sven Strittmatter <ich@weltraumschaf.de>
2014-03-14 21:39:26 +0100

    Refactored contact to meet spec.

---
+++ src/main/java/de/weltraumschaf/dht/Contact.java
@@ -0,0 +1,92 @@
+/*
+ *  LICENSE
+ *
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a non alcohol-free beer in return.
+ *
+ * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
+ */
+
+package de.weltraumschaf.dht;
+
+import com.google.common.base.Objects;
+import de.weltraumschaf.dht.net.NetworkAddress;
+import java.util.UUID;
+import org.apache.commons.lang3.Validate;
+
+/**
+ * The data how a node is reachable.
+ * <p>
+ * This entity is immutable and will be stored in the DHT.
+ * </p>
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ */
+public final class Contact {
+
+    /**
+     * Global unique user id.
+     */
+    private final UUID nodeId;
+    /**
+     * Host on which the users client listen.
+     */
+    private final NetworkAddress networkAddress;
+
+    /**
+     * Dedicated constructor.
+     *
+     * @param userId must not be {@code null}
+     * @param networkAddress must not be {@code null}
+     */
+    public Contact(final UUID userId, final NetworkAddress networkAddress) {
+        super();
+        this.nodeId = Validate.notNull(userId, "Parameter >userId< must not be null!");
+        this.networkAddress = Validate.notNull(networkAddress, "Parameter >networkAddress< must not be null!");
+    }
+
+    /**
+     * Unique id of node.
+     *
+     * @return never {@code null}
+     */
+    public UUID getNodeId() {
+        return nodeId;
+    }
+
+    /**
+     * Get the network address.
+     *
+     * @return never {@code null}
+     */
+    public NetworkAddress getNetworkAddress() {
+        return networkAddress;
+    }
+
+    @Override
+    public String toString() {
+        return Objects.toStringHelper(this).add("nodeId", nodeId).add("networkAddress", networkAddress).toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(nodeId, networkAddress);
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (!(obj instanceof Contact)) {
+            return false;
+        }
+
+        final Contact other = (Contact) obj;
+        return Objects.equal(nodeId, other.getNodeId())
+            && Objects.equal(networkAddress, other.getNetworkAddress());
+    }
+
+
+
+}
